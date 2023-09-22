923139b9f8a7ede426ec271d983dd5b0336f753e
Connor Clark <cjamcl@gmail.com>
2023-09-22 16:46:24 -0700

    wip

--- scripts/update-zscript-docs.sh
+++ scripts/update-zscript-docs.sh
@@ -2,18 +2,27 @@
 
 set -ex
 
-mkdir -p .tmp
-git clone --depth=1 https://github.com/ZQuestClassic/ZQuestClassic.git .tmp/ZQuestClassic || true
-cd .tmp/ZQuestClassic
-git pull
+mkdir -p .tmp/zscript
 
-if [[ "$NETLIFY" ]]; then
-    brew install python3
-    PYTHON=/home/linuxbrew/.linuxbrew/bin/python3
-else
-    PYTHON=python
+curl -L https://api.github.com/repos/ZQuestClassic/ZQuestClassic/commits?path=webdocs > .tmp/latest.json
+SHA=$(jq -r '.[0].sha' .tmp/latest.json)
+
+if [ ! -d .tmp/zscript/$SHA.html ]; then
+    git clone --depth=1 https://github.com/ZQuestClassic/ZQuestClassic.git .tmp/ZQuestClassic || true
+    cd .tmp/ZQuestClassic
+    git pull
+
+    if [[ "$NETLIFY" ]]; then
+        brew install python3
+        PYTHON=/home/linuxbrew/.linuxbrew/bin/python3
+    else
+        PYTHON=python
+    fi
+
+    $PYTHON -m pip install pytz
+    mkdir -p ../../content/docs/zscript
+    $PYTHON webdocs/docjson_main.py --in webdocs/zsdocs_main.json --out ../../.tmp/zscript/$SHA.html -c
+    cd ../..
 fi
 
-$PYTHON -m pip install pytz
-mkdir -p ../../content/docs/zscript
-$PYTHON webdocs/docjson_main.py --in webdocs/zsdocs_main.json --out ../../content/docs/zscript/index.html -c
+mv .tmp/zscript/$SHA.html content/docs/zscript/index.html
