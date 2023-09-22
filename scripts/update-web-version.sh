b4bc4ce0a64dd27db7ddcdacb0df566c59aa5ca7
Connor Clark <cjamcl@gmail.com>
2023-09-22 16:29:26 -0700

    wip

--- scripts/update-web-version.sh
+++ scripts/update-web-version.sh
@@ -2,19 +2,24 @@
 
 set -ex
 
-mkdir -p .tmp
+mkdir -p .tmp/web
 
-curl -L https://api.github.com/repos/ArmageddonGames/ZQuestClassic/releases/latest > .tmp/latest.json
-URL=$(jq -r '.assets[] | select(.name|endswith("web.zip")) | .browser_download_url' .tmp/latest.json)
-wget $URL -O .tmp/web.zip
-unzip .tmp/web.zip -d .tmp/web
-
-rm -rf _site/{play,create,timidity}
-rm .tmp/web/dist/index.html
-mv .tmp/web/dist/* _site
-sed -i 's@/zc/@/@g' _site/create/index.html || (sed 's@/zc/@/@g' _site/create/index.html > out.html && mv out.html _site/create/index.html)
-sed -i 's@/zc/@/@g' _site/play/index.html || (sed 's@/zc/@/@g' _site/play/index.html > out.html && mv out.html _site/play/index.html)
-npx --yes json -I -f _site/manifest.json -e "this.scope='https://zquestclassic.com'"
 cp _headers _site
 
-rm -rf .tmp/web.zip .tmp/web .tmp/latest.json
+curl -L https://api.github.com/repos/ZQuestClassic/ZQuestClassic/releases/latest > .tmp/latest.json
+VERSION=$(jq -r '.tag_name' .tmp/latest.json)
+
+if [ ! -d .tmp/web/release-$VERSION ]; then
+    URL=$(jq -r '.assets[] | select(.name|endswith("web.zip")) | .browser_download_url' .tmp/latest.json)
+    wget $URL -O .tmp/web.zip
+    unzip .tmp/web.zip -d .tmp/web
+
+    rm .tmp/web/dist/index.html
+    sed -i 's@/zc/@/@g' .tmp/web/dist/create/index.html || (sed 's@/zc/@/@g' .tmp/web/dist/create/index.html > out.html && mv out.html .tmp/web/dist/create/index.html)
+    sed -i 's@/zc/@/@g' .tmp/web/dist/play/index.html || (sed 's@/zc/@/@g' .tmp/web/dist/play/index.html > out.html && mv out.html .tmp/web/dist/play/index.html)
+    npx --yes json -I -f .tmp/web/dist/manifest.json -e "this.scope='https://zquestclassic.com'"
+    mv .tmp/web/dist .tmp/web/release-$VERSION
+fi
+
+cp -r .tmp/web/release-$VERSION/* _site
+rm -rf .tmp/web.zip .tmp/latest.json
