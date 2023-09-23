74ac38e08eb68aa2934c2fd499c9c9732a8a5c52
Connor Clark <cjamcl@gmail.com>
2023-09-22 17:06:30 -0700

    wip

--- scripts/update-zscript-docs.sh
+++ scripts/update-zscript-docs.sh
@@ -7,7 +7,7 @@ mkdir -p .tmp/zscript
 curl -L https://api.github.com/repos/ZQuestClassic/ZQuestClassic/commits?path=webdocs > .tmp/latest.json
 SHA=$(jq -r '.[0].sha' .tmp/latest.json)
 
-if [ ! -d .tmp/zscript/$SHA.html ]; then
+if [ ! -f .tmp/zscript/$SHA.html ]; then
     git clone --depth=1 https://github.com/ZQuestClassic/ZQuestClassic.git .tmp/ZQuestClassic || true
     cd .tmp/ZQuestClassic
     git pull
