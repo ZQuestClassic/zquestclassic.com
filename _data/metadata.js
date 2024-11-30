54635be36f4d95683350d6f8e3b6ab896147287e
Connor Clark <cjamcl@gmail.com>
2024-11-30 13:58:20 -0800

    tweak description

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,10 +2,5 @@ module.exports = {
 	title: "ZQuest Classic",
 	url: "https://zquestclassic.com/",
 	language: "en",
-	description: "ZQuest Classic - The Adventure Game Engine",
-	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
-	}
+	description: "ZQuest Classic - The Adventure Game Engine (formerly known as Zelda Classic)"
 }
